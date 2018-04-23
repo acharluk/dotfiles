23d3ffbae0944c3cba136518f946294ca5f35e83
ACharLuk <acharluk@gmail.com>
2018-04-23 14:53:10 +0000

    Setup yaourt and packages

--- setup.sh
+++ setup.sh
@@ -1,2 +1,15 @@
 # Just a shortcut (sudo)
 echo "\nalias x=\"startx\"" >> /etc/bash.bashrc
+
+# Yaourt
+echo " " >> /etc/pacman.conf
+echo "[archlinuxfr]" >> /etc/pacman.conf
+echo "SigLevel = Never" >> /etc/pacman.conf
+echo "Server = http://repo.archlinux.fr/\$arch" >> /etc/pacman.conf
+
+# Update pacman and install yaourt
+pacman -Sy yaourt --noconfirm
+
+# Install packages
+pacman -S $(cat packagelist)
+pacman -S $(cat packagelist.optional)
