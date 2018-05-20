e26fda8f9f7a4a53a1e0bbcf5c5c0219cb57ead5
ACharLuk <acharluk@gmail.com>
2018-05-20 12:14:05 +0200

    Create screenshots directory and user repo folder

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ current_date=$(date +"%F_%H-%M-%S")
 backup_folder=~/.acharluk/backups/$current_date
 
 echo "Creating home directories"
-mkdir -p ~/testing ~/downloads ~/repos ~/documents ~/music ~/pictures ~/videos
+mkdir -p ~/testing ~/downloads ~/repos/$USER ~/documents ~/music ~/pictures/screenshots ~/videos
 
 sudo sh installpackages.sh
 
